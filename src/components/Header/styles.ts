e3891549e10b2bdfe4f25d668a17a4646ec7dbe0
Davis Sampaio <davislamenha@gmail.com>
2023-09-21 13:56:08 -0300

    imagens adicionadas, e componente Header criado

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+import { Cores } from '../../styles'
+
+export const HeaderBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: ${Cores.cinza};
+  padding: 24px;
+  border-radius: 16px;
+  margin-bottom: 80px;
+  align-items: center;
+
+  a {
+    color: ${Cores.branco};
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+`
+export const Links = styled.ul`
+  display: flex;
+  margin-left: 40px;
+`
+export const LinkItem = styled.li`
+  margin-right: 16px;
+`
+export const LinkCart = styled.a`
+  display: flex;
+
+  img {
+    margin-left: 16px;
+  }
+`
--- src/styles.ts
+++ src/styles.ts
@@ -1,22 +1,31 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const Cores = {
+export const Cores = {
   branco: '#EEE',
   preta: '#111',
   cinza: '#333',
   verde: '#10AC84'
 }
 
+export const Container = styled.div`
+  margin: 0 auto;
+  width: min(100% - 30px, 1440px);
+`
+
 export const GlobalCss = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-size: 'Roboto', sans-serif;
+  font-family: 'Roboto', sans-serif;
+  list-style: none;
 }
 
 body {
   background-color: ${Cores.preta};
   color: ${Cores.branco};
+  padding-top: 40px;
 }
+
+
 `
