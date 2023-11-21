b293e0855efc7ad9b49e142829ee313e0fc7c932
Davis Sampaio <davislamenha@gmail.com>
2023-11-21 11:36:12 -0300

    construindo hero da pagina produto

---
+++ src/pages/Product/index.tsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+import Hero from '../../components/Hero'
+
+const Product = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default Product
